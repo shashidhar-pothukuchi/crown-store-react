8179775bb8dac3526720e81748cd86b3c7ab089d
sai <saithechinnu@gmail.com>
2019-09-28 11:37:16 +0530

    Homepage commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import './components/HomePage'
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div>
-      
+      <HomePage />
     </div>
   );
 }
